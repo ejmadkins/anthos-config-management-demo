1b30fb29c340027a163b18d8c9eb21587c010fe2
Ed Adkins <ejmadkins@google.com>
2020-06-25 11:55:03 +0100

    add cloud env variable

--- src/app.py
+++ src/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template
 import requests
+import os
  
 app = Flask(__name__)
 
 @app.route('/')
 def t_rex_runner():
-    return render_template('index.html')
+    return render_template('index.html', CLOUD_ENV=os.environ.get('environment'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
