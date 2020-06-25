7aa55b7e31df1e79143824e76f921d36778a4261
Ed Adkins <ejmadkins@google.com>
2020-06-25 12:51:51 +0100

    add deployment and services

--- src/app.py
+++ src/app.py
@@ -5,9 +5,11 @@ import os
  
 app = Flask(__name__)
 
+CLOUD_ENV=os.environ.get('environment')
+
 @app.route('/')
 def t_rex_runner():
-    return render_template('index.html', CLOUD_ENV=os.environ.get('environment'))
+    return render_template('index.html', CLOUD_ENV=CLOUD_ENV)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
